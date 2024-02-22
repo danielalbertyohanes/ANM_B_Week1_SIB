9cf8e850bd6692d67720a6b6a9d56d08b31e329e
Daniel _ <danieltmx333@gmail.com>
2024-02-22 14:29:49 +0700

    operation activity update

--- app/src/main/java/com/example/anmp_week1/OperationActivity.kt
+++ app/src/main/java/com/example/anmp_week1/OperationActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class OperationActivity : AppCompatActivity() {
+
+    //ALOHAA
+    //Hallo
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_operation)
